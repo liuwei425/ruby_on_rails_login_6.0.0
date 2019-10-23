4357596b8aca7a28ccaa9e1acd4a7ec0cf7bca7f
admiral <admiral1ap0901@gmail.com>
2019-10-23 04:51:44 -0700

    users guard complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-class PostsController < ApplicationController
+class PostsController < HomeController
     before_action :find_post, only: [:show, :edit, :update, :destroy]
     before_action :check_admin, only: [:index]
     def index
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController < ApplicationController
+class SessionsController < HomeController
     def new
     end
 
